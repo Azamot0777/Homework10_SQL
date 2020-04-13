a441f941552c3ea3d5379c83857df68811dbab54
Heisenberg <Persey_98@list.ru>
2020-04-13 21:20:32 +0500

    I've created class sql and method getInfo and other things

--- Program.cs
+++ Program.cs
@@ -2,11 +2,34 @@
 
 namespace Homework10_SQL
 {
+    public class SQL
+    {
+        public string Name;
+        public string Size;
+        public string Model;
+
+        public SQL() { }
+        public void GetInfo()
+        {
+            System.Console.WriteLine($"Название Флешки : {Name}");
+            System.Console.WriteLine($"Размер Флешки : {Size}");
+            System.Console.WriteLine($"Модель Флешки : {Model}");
+        }
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] name = { "Флешка для компа", "Флешка для смартфона" };
+            string[] size = { "32 gb", "64 gb", "128 gb", "256 gb", "512 gb" };
+            string[] model = { "3211p", "11o1k", "2o20p", };
+            for (int i = 0; i < 9; i++)
+            {
+                SQL q = new SQL();
+            }
+
         }
     }
 }
